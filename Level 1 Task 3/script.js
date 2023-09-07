1b0a08425789dacdb8d7a6f20fecf7cf992b8c6e
Vanamali Sedimbi <128423291+vanamalisedimbi@users.noreply.github.com>
2023-09-07 22:34:00 +0530

    Add files via upload

---
+++ Level 1 Task 3/script.js
@@ -0,0 +1,16 @@
+let string = "";
+let buttons = document.querySelectorAll('.button, .amber, .grey'); // Include amber and grey classes
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
+      string = eval(string);
+      document.querySelector('input').value = string;
+    } else if (e.target.innerHTML == 'C') {
+      string = "";
+      document.querySelector('input').value = string;
+    } else {
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
+    }
+  });
+});
